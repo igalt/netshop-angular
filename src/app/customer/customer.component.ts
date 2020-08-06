8eb0caf7cdf161d866c561e25e3ef7023044bc36
Igal Tar <igal.tar@gmail.com>
2020-08-06 20:11:20 +0300

    customer component

---
+++ src/app/customer/customer.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+import { DataService } from '../data.service';
+import { Customer } from '../models/customer';
+import { ShoppingCart } from '../models/shoppingCart';
+
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.css']
+})
+export class CustomerComponent implements OnInit {
+
+  customer: Customer;
+  cart: ShoppingCart;
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.dataService.getCustomer()
+                    .then(customer => this.customer = customer);
+    
+    this.cart = this.dataService.cart;
+  }
+
+}
