f5c255a30a004004d1bae7a1f6c997ab7425eda7
Igal Tar <igal.tar@gmail.com>
2020-08-16 20:19:27 +0300

    prepare Heroku

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 const path = require('path'); 
+const cors = require('cors');
 
 const products = require('./products');
 const customers = require('./customers');
